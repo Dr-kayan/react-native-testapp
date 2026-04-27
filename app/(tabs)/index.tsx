9ed75658a11608f485d07fda0d7a36906312f230
Dr-kayan <kayanndabaja@gmail.com>
2026-04-28 02:28:45 +0300

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,32 +9,26 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
     return (
         <SafeAreaView className="flex-1 bg-background p-5">
-            <Text className="text-xl font-bold text-blue-500">
-                Welcome to Nativewind!
+            <Text className="text-7xl font-bold font-sans-extrabold">
+                Home
             </Text>
-            <Link href="/onboarding" className="mt-4 rounded bg-primary text-amber-800 p-4">
+            <Text className="text-7xl font-bold ">
+                Home
+            </Text>
+
+            <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary text-amber-800 p-4">
                 Go to onboarding
             </Link>
 
-            <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-amber-800 p-4">
-                Sign in with existing account
+            <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-amber-800 p-4">
+                Sign In with Existing Account
             </Link>
 
-            <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-amber-950 p-4">
-                Sign Up with new account
+            <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-amber-800 p-4">
+                Sign Up with New Account
             </Link>
 
 
-            <Link href="/subscriptions/spotify" className="mt-4 rounded bg-primary text-amber-950 p-4">
-                Spotify Sub
-            </Link>
-
-            <Link href={{
-                pathname: '/subscriptions/[id]',
-                params: { id : 'kayan'}
-            }} className="mt-4 rounded bg-primary text-amber-950 p-4">
-                kayan sub
-            </Link>
 
 
         </SafeAreaView>
